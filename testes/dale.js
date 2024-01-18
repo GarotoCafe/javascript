0c382ec6e269200a593cf7eb6fe2126f483f119c
Régis <114204678+GarotoCafe@users.noreply.github.com>
2024-01-18 20:43:35 -0300

    teste dos exercícios do curso da ONE

---
+++ testes/dale.js
@@ -0,0 +1,6 @@
+let numeroMaximo = prompt("Digite um número, para a contagem regressiva começar:");
+
+while (numeroMaximo => 0){
+    console.log(numeroMaximo);
+    numeroMaximo++;
+}
---
+++ testes/fruta.js
@@ -0,0 +1,8 @@
+let tmaca = false;
+let tbanana = true;
+
+if (tmaca || tbanana) {
+    console.log("Você tem frutas!");
+} else {
+    console.log("Você não tem frutas :( ");
+}
